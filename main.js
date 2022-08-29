8070ef8ffb67863e759d9beed1bedf447323d57e
sizonyuk.a@gmail.com <sizonyuk.a@gmail.com>
2022-08-29 21:00:56 +0300

    Add calculator functions

--- main.js
+++ main.js
@@ -1,74 +1,134 @@
-const calcInput = document.querySelector('.display')
-const allButtons = document.querySelectorAll('.button')
 
-// Add values on display
+function calculator() {
+	// Selectors
+	const previous = document.querySelector(".previous")
+	const current = document.querySelector(".current")
+	const numbers = document.querySelectorAll(".number")
+	const operations = document.querySelectorAll(".operation")
+	const clear = document.querySelector(".clear")
+	const deleteButton = document.querySelector(".delete")
 
-allButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const value = event.target.dataset.value
-    if (value === '=') {
-      if (calcInput.innerText.includes('/')) {
-        const splitData = calcInput.innerText.split('/')
-        operate('/', splitData[0], splitData[1])
-      }
-      if (calcInput.innerText.includes('-')) {
-        const splitData = calcInput.innerText.split('-')
-        operate('-', splitData[0], splitData[1])
-      }
-      if (calcInput.innerText.includes('+')) {
-        const splitData = calcInput.innerText.split('+')
-        operate('+', splitData[0], splitData[1])
-      }
-      if (calcInput.innerText.includes('*')) {
-        const splitData = calcInput.innerText.split('*')
-        operate('*', splitData[0], splitData[1])
-      }
-    }
+	const equal = document.querySelector(".equals")
+
+	let currentOperand = ""
+	let previousOperand = ""
+	let operation = undefined
 
 
-    if (calcInput.innerText.length <= 8) {
-      calcInput.innerText += value
-      calcInput.innerText = calcInput.innerText.replace('=', '')
-    }
+	// Operation with buttons 
+	function handleButtons() {
+		// eventListener for numbers  
+		numbers.forEach(button => {
+			button.addEventListener("click", () => {
+				currentOperand === 0 ? currentOperand = "" : ''
+				console.log(button)
+				if (button.textContent === "." && currentOperand.includes(".")) return
+				currentOperand += button.textContent.toString()
+				console.log(currentOperand)
+				updateDisplay()
+			})
+		})
 
-	 if (calcInput.innerText.startsWith('/')) {
-		calcInput.innerText = ''
-	 }
-	 else if (calcInput.innerText.startsWith('*')) {
-		calcInput.innerText = ''
-	 }
-	//  else if (calcInput.innerText.startsWith('-')) {
-	// 	calcInput.innerText = ''
-	//  }
-	 else if (calcInput.innerText.startsWith('+')) {
-		calcInput.innerText = ''
-	 }
+		// eventlistener for operations 
+		operations.forEach(operationButtons => {
+			operationButtons.addEventListener("click", () => {
+				console.log(operationButtons)
+				if (currentOperand === "") return
+				operation = operationButtons.textContent
+				mathematicalOperations()
+				updateDisplay()
+			})
+		})
 
 
-    if (value === 'clear') {
-      calcInput.innerText = ''
-    }
-  }
-  
- 
-  )
-})
+		// eventlistener for clear button 
 
-const operate = (operator, value1, value2) => {
-  const num1 = Number(value1)
-  const num2 = Number(value2)
 
-  if (operator === '*') {
-    calcInput.innerText = num1 * num2
-  }
 
-  else if (operator === '+') {
-    calcInput.innerText = num1 + num2
-  }
-  else if (operator === '-') {
-    calcInput.innerText = num1 - num2
-  }
-  else if (operator === '/') {
-    calcInput.innerText = num1 / num2
-  }
+		clear.addEventListener("click", () => {
+			currentOperand = 0
+			previousOperand = " "
+			operation = undefined
+			updateDisplay()
+
+		})
+
+
+		// eventlistener for delete button 
+		deleteButton.addEventListener("click", () => {
+			let temporaryValue
+
+			if (currentOperand === "Division by 0 is not allowed") {
+				currentOperand = 0
+				temporaryValue = currentOperand
+			}
+
+			else {
+				temporaryValue = currentOperand.toString().slice(0, -1)
+			}
+
+			if (temporaryValue === "" || temporaryValue === 0) {
+				temporaryValue = 0
+				currentOperand = temporaryValue
+				updateDisplay()
+			}
+			else {
+				currentOperand = parseFloat(temporaryValue)
+				updateDisplay()
+			}
+
+		})
+
+		equal.addEventListener("click", () => {
+			calculateResult()
+			updateDisplay()
+
+		})
+
+	}
+
+
+	function mathematicalOperations() {
+		if (currentOperand === "") return
+		if (previousOperand !== "") {
+			calculateResult()
+		}
+
+		previousOperand = `${currentOperand}${operation}`
+		currentOperand = ""
+	}
+
+
+	function calculateResult() {
+		let currentValue = parseFloat(currentOperand)
+		let previousValue = parseFloat(previousOperand)
+		let result
+		if (isNaN(currentValue) || isNaN(previousValue)) return
+		operation === "+" ? result = currentValue + previousValue
+			: operation === "-" ? result = previousValue - currentValue
+				: operation === "/" && currentValue === 0 ? result = "Division by 0 is not allowed"
+
+					: operation === "/" ? result = previousValue / currentValue
+
+						: operation === "*" ? result = previousValue * currentValue
+
+							: ""
+
+
+		currentOperand = result
+		operation = undefined
+		previousOperand = ""
+	}
+
+
+	function updateDisplay() {
+
+		current.textContent = currentOperand
+		previous.textContent = previousOperand
+
+	}
+	handleButtons()
+
 }
+
+calculator()
